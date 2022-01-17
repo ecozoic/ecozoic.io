3ae4c430eff7e8340b034d1fcbe6999761cedfd1
ecozoic <johnnn.mitchell@gmail.com>
2022-01-17 09:27:43 -0800

    add some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,24 @@
-module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
-    ]
-}
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.ecozoic.io',
+  },
+  plugins: [
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
+};
